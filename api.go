7cb65ae7f42a8076585e78a6b62584cf03fc1dc6
Brian Rosner <brian@brosner.com>
2015-09-03 17:45:01 -0600

    export Persister for use

--- api.go
+++ api.go
@@ -19,11 +19,11 @@ type ClientOpts struct {
 		AccessToken  string
 		RefreshToken string
 	}
-	persister ClientOptsPersister
+	Persister ClientOptsPersister
 }
 
 func (opts *ClientOpts) Persist() error {
-	return opts.persister.Persist(opts)
+	return opts.Persister.Persist(opts)
 }
 
 type Client struct {
